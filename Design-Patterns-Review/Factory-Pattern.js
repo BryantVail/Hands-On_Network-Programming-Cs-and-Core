67f173218c6e54bbe32591943b1eb207319ca3e4
Bryant Vail <Bryanttv@outlook.com>
2019-08-26 19:54:24 -0400

    Create Factory-Pattern.js
    notes on FactoryPattern.js

---
+++ Design-Patterns-Review/Factory-Pattern.js
@@ -0,0 +1,8 @@
+({
+	name: "Factory-Pattern",
+	desc:
+		"Defines an interface for creating an object, but lets the classes that 'implement' the interface decide which class to instantiate; enables a class to postpone instantiation to sub-classes.",
+	notes: [
+		"replaces 'constructors'; abstracting the process of object generation so that the type of the object instantiated can be determined at 'run-time'."
+	]
+});
